8e554b1349cf0f6e03d7cbd6487085bf710a585e
lillebrg <LilleBRG@gmail.com>
2022-02-24 08:17:02 +0100

    program cs updated

--- FagTilmedlingApp/Program.cs
+++ FagTilmedlingApp/Program.cs
@@ -1,1 +1,12 @@
-﻿//Iterarion1
+﻿// Iteration 1
+string SkoleInput, HovedforlobInput;
+Console.WriteLine("Angiv skole: ");
+SkoleInput = Console.ReadLine();
+Console.WriteLine("Angiv hovedforløb: ");
+HovedforlobInput = Console.ReadLine();
+Semester se = new(SkoleInput, HovedforlobInput);
+Console.Clear();
+
+Console.WriteLine("-------------------------------------------------");
+Console.WriteLine("{0}, {1} fag tildmelding app.", se.SchoolName, se.SemesterNavn);
+Console.WriteLine("-------------------------------------------------");
